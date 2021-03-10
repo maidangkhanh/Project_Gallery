b8777f74ddbb3813005af4e5a4383e975dabb2cd
maidangkhanh <mai.d.khanh@gmail.com>
2021-03-10 18:11:22 +0700

    ViewByDate Layout

---
+++ app/src/main/java/com/example/gallery/ViewByDate.java
@@ -0,0 +1,14 @@
+package com.example.gallery;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ViewByDate  extends  AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_by_date);
+    }
+}
